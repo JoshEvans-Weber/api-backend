aeb8ed040913aa7f5b8eaf8bdaf889be5b7afc2e
Joshua Evans <joshuaevans5@mail.weber.edu>
2025-05-24 04:09:39 -0600

    modified:   docker-compose.yml

--- migrations/2025-05-24_add_google_id_to_users.sql
+++ migrations/2025-05-24_add_google_id_to_users.sql
@@ -1,3 +1,5 @@
 -- Migration: Add google_id column to users table for Google+PIN parent registration
+ALTER TABLE users DROP COLUMN IF EXISTS google_id;
+DROP INDEX IF EXISTS idx_users_google_id ON users;
 ALTER TABLE users ADD COLUMN google_id VARCHAR(128) DEFAULT NULL;
 CREATE UNIQUE INDEX IF NOT EXISTS idx_users_google_id ON users(google_id);
